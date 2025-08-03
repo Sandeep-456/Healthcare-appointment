7cee2f0fe6dfbc5d5841aba2e581d92869288cfb
Sandeep-456 <sandeepindugula456@gmail.com>
2025-08-04 03:33:27 +0530

    changed front-end-url

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,21 @@ const appointmentsRoute = require("./routes/appointment");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ['https://healthcare-appointment-seven.vercel.app/', 'http://localhost:5173'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+const allowedOrigins = [
+  "https://healthcare-appointment-seven.vercel.app",
+  "http://localhost:5173",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(bodyParser.json());
 app.use(express.json());
 
